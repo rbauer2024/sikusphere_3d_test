04730e44feb23ea9c4702a45e02fe337754b7df5
rbauer2024 <168412926+rbauer2024@users.noreply.github.com>
2025-07-01 08:55:49 +0200

    Update app.js

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.set(-3.5, 1.1, 3.5); // etwas mehr seitlich      // X = 0 (zentral), Y = 0.4 (etwas über dem Boden), Z = 4.5 (Abstand)
-camera.lookAt(0, 0.15, 0);             // Blickpunkt bleibt leicht nach oben gerichtet
+camera.position.set(-4.2, 1.0, 3.0); // etwas mehr seitlich      // X = 0 (zentral), Y = 0.4 (etwas über dem Boden), Z = 4.5 (Abstand)
+camera.lookAt(0, 0.1, 0);             // Blickpunkt bleibt leicht nach oben gerichtet
 
 // Renderer mit hoher Schärfe
 const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
